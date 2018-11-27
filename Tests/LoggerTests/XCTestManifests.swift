33bf4e766fb47557bb310413e2504b8fb09e7c73
Franco Meloni <franco.meloni91@gmail.com>
2018-11-27 18:45:42 +0000

    Generate linuxmain

--- Tests/LoggerTests/XCTestManifests.swift
+++ Tests/LoggerTests/XCTestManifests.swift
@@ -1,9 +1,34 @@
 import XCTest
 
+extension LoggerTests {
+    static let __allTests = [
+        ("testItDoentPrintTheDebugMessageIfIsSilent", testItDoentPrintTheDebugMessageIfIsSilent),
+        ("testItDoentPrintTheErrorMessageIfIsSilent", testItDoentPrintTheErrorMessageIfIsSilent),
+        ("testItDoentPrintTheInfoMessageIfIsSilent", testItDoentPrintTheInfoMessageIfIsSilent),
+        ("testItDoentPrintTheWarningMessageIfIsSilent", testItDoentPrintTheWarningMessageIfIsSilent),
+        ("testItDoesntPrintTheDebugMessageIfTheMessageIsVerboseAndTheLoggerIsNotInVerboseMode", testItDoesntPrintTheDebugMessageIfTheMessageIsVerboseAndTheLoggerIsNotInVerboseMode),
+        ("testItDoesntPrintTheErrorMessageIfTheMessageIsVerboseAndTheLoggerIsNotInVerboseMode", testItDoesntPrintTheErrorMessageIfTheMessageIsVerboseAndTheLoggerIsNotInVerboseMode),
+        ("testItDoesntPrintTheInfoMessageIfTheMessageIsVerboseAndTheLoggerIsNotInVerboseMode", testItDoesntPrintTheInfoMessageIfTheMessageIsVerboseAndTheLoggerIsNotInVerboseMode),
+        ("testItDoesntPrintTheWarningMessageIfTheMessageIsVerboseAndTheLoggerIsNotInVerboseMode", testItDoesntPrintTheWarningMessageIfTheMessageIsVerboseAndTheLoggerIsNotInVerboseMode),
+        ("testItPrintsTheDebugMessageIfTheMessageIsNotVerboseAndTheLoggerIsInVerboseMode", testItPrintsTheDebugMessageIfTheMessageIsNotVerboseAndTheLoggerIsInVerboseMode),
+        ("testItPrintsTheDebugMessageIfTheMessageIsNotVerboseAndTheLoggerIsNotVerboseMode", testItPrintsTheDebugMessageIfTheMessageIsNotVerboseAndTheLoggerIsNotVerboseMode),
+        ("testItPrintsTheDebugMessageIfTheMessageIsVerboseAndTheLoggerIsInVerboseMode", testItPrintsTheDebugMessageIfTheMessageIsVerboseAndTheLoggerIsInVerboseMode),
+        ("testItPrintsTheErrorMessageIfTheMessageIsNotVerboseAndTheLoggerIsInVerboseMode", testItPrintsTheErrorMessageIfTheMessageIsNotVerboseAndTheLoggerIsInVerboseMode),
+        ("testItPrintsTheErrorMessageIfTheMessageIsNotVerboseAndTheLoggerIsNotVerboseMode", testItPrintsTheErrorMessageIfTheMessageIsNotVerboseAndTheLoggerIsNotVerboseMode),
+        ("testItPrintsTheErrorMessageIfTheMessageIsVerboseAndTheLoggerIsInVerboseMode", testItPrintsTheErrorMessageIfTheMessageIsVerboseAndTheLoggerIsInVerboseMode),
+        ("testItPrintsTheInfoMessageIfTheMessageIsNotVerboseAndTheLoggerIsInVerboseMode", testItPrintsTheInfoMessageIfTheMessageIsNotVerboseAndTheLoggerIsInVerboseMode),
+        ("testItPrintsTheInfoMessageIfTheMessageIsNotVerboseAndTheLoggerIsNotVerboseMode", testItPrintsTheInfoMessageIfTheMessageIsNotVerboseAndTheLoggerIsNotVerboseMode),
+        ("testItPrintsTheInfoMessageIfTheMessageIsVerboseAndTheLoggerIsInVerboseMode", testItPrintsTheInfoMessageIfTheMessageIsVerboseAndTheLoggerIsInVerboseMode),
+        ("testItPrintsTheWarningMessageIfTheMessageIsNotVerboseAndTheLoggerIsInVerboseMode", testItPrintsTheWarningMessageIfTheMessageIsNotVerboseAndTheLoggerIsInVerboseMode),
+        ("testItPrintsTheWarningMessageIfTheMessageIsNotVerboseAndTheLoggerIsNotVerboseMode", testItPrintsTheWarningMessageIfTheMessageIsNotVerboseAndTheLoggerIsNotVerboseMode),
+        ("testItPrintsTheWarningMessageIfTheMessageIsVerboseAndTheLoggerIsInVerboseMode", testItPrintsTheWarningMessageIfTheMessageIsVerboseAndTheLoggerIsInVerboseMode),
+    ]
+}
+
 #if !os(macOS)
-public func allTests() -> [XCTestCaseEntry] {
+public func __allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(LoggerTests.allTests),
+        testCase(LoggerTests.__allTests),
     ]
 }
 #endif
